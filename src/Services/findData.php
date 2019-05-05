565f13bff2995b7a0b226333773e52134753b619
arthur <richarthur@gmail.com>
2019-05-05 11:45:43 +0200

    image find data

--- src/Services/findData.php
+++ src/Services/findData.php
@@ -51,14 +51,14 @@ class findData
 
     public function setDefaultContent ($user, $entityManager) {
         $newAbout = new About();
-        $fileAbout = ($_SERVER['DOCUMENT_ROOT']  . 'public/images/about/how-to-create-a-blog.png');
+        $fileAbout = ($_SERVER['DOCUMENT_ROOT']  . 'images/about/how-to-create-a-blog.png');
         $imageAbout = new UploadedFile($fileAbout, $fileAbout, null, false, true);
         $newAbout->setDescription('Personnaliser votre profil') 
                  ->setImageFile($imageAbout);            
         $user->addAbout($newAbout);
 
         $newHeader = new Header();
-        $fileHeader = '../../public/images/header/header.jpg';
+        $fileHeader = ($_SERVER['DOCUMENT_ROOT']  . 'images/header/header.jpg');
         $imageHeader = new UploadedFile($fileHeader, $fileHeader, null, false, true);
         $newHeader->setTitle('Bienvenue sur votre Blog')
                   ->setSubtitle('Commencer à le personnaliser')
